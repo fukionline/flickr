193ee5ffcc35aab371673486c9fdc6a339c1b8ee
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2023-07-31 20:10:16 +0300

    Cleanups

--- upload.php
+++ upload.php
@@ -1,70 +1,6 @@
 <?php 
 require_once($_SERVER["DOCUMENT_ROOT"] . "/incl/header.php"); 
-
-function clone_img_resource($img) {
-    return imagecrop($img, array('x'=>0,'y'=>0,'width'=>imagesx($img),'height'=>imagesy($img)));
-}
-
-function get_aspect_ratio($width, $height) {
-    $gcd = static function($width, $height) use (&$gcd) {
-        return ($width % $height) ? $gcd($height, $width % $height) : $height;
-    };
-    $divisor = $gcd($width, $height);
-    return $width / $divisor . ':' . $height / $divisor;
-}
-
-function letterbox_img($image, $background = false, $canvas_w, $canvas_h) {
-    if (!$background) {
-        $background = imagecolorallocate($image, 255, 255, 255);
-    }
-    $img_h = imagesy($image);
-    $img_w = imagesx($image);
-    $img = imagecreatetruecolor($canvas_w, $canvas_h);
-    imagefill($img, 0, 0, $background);
-    $xoffset = round(($canvas_w - $img_w) / 2);
-    $yoffset = round(($canvas_h - $img_h) / 2);
-    imagecopymerge($img, $image, $xoffset, $yoffset, 0,0, $img_w, $img_h, 100);
-    return $img; 
-}
-
-function orient_img($image, $filename) {
-    $exif = exif_read_data($filename);
-    
-    if (!empty($exif['Orientation'])) {
-        switch ($exif['Orientation']) {
-            case 3:
-               return imagerotate($image, 180, 0);
-                break;
-            
-            case 6:
-                return imagerotate($image, 90, 0);
-                break;
-            
-            case 8:
-                return imagerotate($image, -90, 0);
-                break;
-        }
-    }
-	
-    return $image;
-}
-
- 
-function process_img($img, $width, $height, $fn) {
-	$gdimage_t_h = $height;
-	$gdimage_t = clone_img_resource($img);
-	$gdimage_t_as = get_aspect_ratio(imagesx($gdimage_t), imagesy($gdimage_t));
-	$gdimage_t_as = explode(':', $gdimage_t_as);
-	$gdimage_t_w = round($gdimage_t_as[0]*($height/$gdimage_t_as[1]));
-	if ($gdimage_t_w > $width) {
-		$gdimage_t_h = round($gdimage_t_as[1]*($width/$gdimage_t_as[0]));
-		$gdimage_t_w = $width;
-	}
-	$gdimage_t = imagescale($gdimage_t, $gdimage_t_w, $gdimage_t_h);
-	$gdimage_t = letterbox_img($gdimage_t, false, $width, $height);
-	imagejpeg($gdimage_t, $fn);
-}
-	
+require_once($_SERVER["DOCUMENT_ROOT"] . "/incl/functions.php"); 	
 
 if(isset($_POST["Submit"])) {
 	$title = $_POST["title"];
@@ -117,9 +53,9 @@ if(isset($_POST["Submit"])) {
 			}
 	}
 	
-	$gdimage = orient_img($gdimage, $upload_tgt_preload);
-	process_img($gdimage, 100, 75, "photos/$photo_id.t.jpg");
-	process_img($gdimage, 240, 180, "photos/$photo_id.m.jpg");
+	$gdimage = imgExifOrient($gdimage, $upload_tgt_preload);
+	imgProcess($gdimage, 100, 75, "photos/$photo_id.t.jpg");
+	imgProcess($gdimage, 240, 180, "photos/$photo_id.m.jpg");
 	imagejpeg(imagescale($gdimage, 500), "photos/$photo_id.jpg");
 	
 	$stmt = $conn->prepare("INSERT INTO photos (title, camera, uploaded_by) VALUES (?, ?, ?)");
