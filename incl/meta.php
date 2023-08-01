2e49a366133ee7c0e725f7905a328fe0a416ab14
IdioticSniper <125594105+IdioticSniper@users.noreply.github.com>
2023-07-31 21:38:03 -0400

    fix upload page meta

--- incl/meta.php
+++ incl/meta.php
@@ -2,24 +2,11 @@
 
 $page = array();
 
-if($_SERVER["PHP_SELF"] == "/index.php") {
-	$page["title"] = "Welcome to " . $website["instance_name"] . " - " . "Photo Sharing";
-}
-
-if($_SERVER["PHP_SELF"] == "/photos.php") {
-	$page["title"] = $website["instance_name"] . ": " . "Photos";
-}
-
-if($_SERVER["PHP_SELF"] == "/register.php") {
-	$page["title"] = $website["instance_name"] . ": " . "Register Now";
-}
-
-if($_SERVER["PHP_SELF"] == "/login.php") {
-	$page["title"] = $website["instance_name"] . ": " . "Login";
-}
-
-if($_SERVER["PHP_SELF"] == "/photo.php") {
-	$page["title"] = $website["instance_name"] . ": " . "Photo";
-	}
+if($_SERVER["PHP_SELF"] == "/index.php") { $page["title"] = "Welcome to " . $website["instance_name"] . " - " . "Photo Sharing"; }
+if($_SERVER["PHP_SELF"] == "/photos.php") { $page["title"] = $website["instance_name"] . ": " . "Photos"; }
+if($_SERVER["PHP_SELF"] == "/register.php") { $page["title"] = $website["instance_name"] . ": " . "Register Now"; }
+if($_SERVER["PHP_SELF"] == "/login.php") { $page["title"] = $website["instance_name"] . ": " . "Login"; }
+if($_SERVER["PHP_SELF"] == "/photo.php") { $page["title"] = $website["instance_name"] . ": " . "Photo"; }
+if($_SERVER["PHP_SELF"] == "/upload.php") { $page["title"] = $website["instance_name"] . ": " . "Upload"; }
 
 ?>
