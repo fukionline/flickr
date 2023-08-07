1b792cb59af8b8fe448edb34b8c81e594b0a057e
IdioticSniper <125594105+IdioticSniper@users.noreply.github.com>
2023-08-07 00:04:37 -0400

    quickly add stuff before dudebloke comes back

--- account.php
+++ account.php
@@ -4,7 +4,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/incl/logincheck.php");
 ?>
 <p>this page will be revamped soon</p>
 <ul>
-	<li><a href="#">Buddy icon builder</a></li>
+	<li><a href="iconbuilder.php">Buddy icon builder</a></li>
 </ul>
 
 <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/incl/footer.php"); ?>
--- incl/functions.php
+++ incl/functions.php
@@ -123,4 +123,8 @@ function getIP()
     return $ip;
 }
 
+function make_links_clickable($text){
+	return preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text);
+}
+
 ?>
